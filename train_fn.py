512fcd4a90a871b74166d169b8a850d4dd6ceba1
Aamer Abdul <ar.aamer@gmail.com>
2022-04-21 13:03:58 +0100

    direcs

--- train_fn.py
+++ train_fn.py
@@ -51,7 +51,17 @@ def main():
     if not os.path.exists(save_path):
         os.mkdir(save_path)
 
-    
+    data_path = 'dataset/camylon/'
+
+    train_feat_path = data_path + 'camelyonpatch_level_2_split_train_x.h5'
+    train_lbl_path = data_path + 'camelyonpatch_level_2_split_train_y.h5'
+
+    test_feat_path = data_path + 'camelyonpatch_level_2_split_test_x.h5'
+    test_lbl_path = data_path + 'camelyonpatch_level_2_split_test_y.h5'
+
+    test_feat_path = data_path + 'camelyonpatch_level_2_split_val_x.h5'
+    test_lbl_path = data_path + 'camelyonpatch_level_2_split_val_y.h5'
+        
     transform = transforms.Compose([transforms.ToPILImage(), 
                                 transforms.ToTensor()])
     target_transform = transforms.Compose([transforms.ToPILImage(), 
